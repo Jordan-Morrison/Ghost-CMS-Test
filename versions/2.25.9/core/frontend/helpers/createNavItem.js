8f1de075df7cf4508def76b2f3ca1c4ca911850a
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2019-08-28 10:54:08 -0400

    Share Options, Footer, Social Accounts
    Updated the sharing options, footer content and social accounts. Now using localstorage to set a boolean 'langIsEnglish' in order to do translation throughout the site.

--- versions/2.25.9/core/frontend/helpers/createNavItem.js
+++ versions/2.25.9/core/frontend/helpers/createNavItem.js
@@ -7,5 +7,15 @@ module.exports = function createNavItem(slug, current, url, label, options) {
 
     let navItem = `<li class="nav-${slug} ${navCurrent}"><a href="${url}">${label}</a></li>`;
 
+    // If the URL is / we know the language should be english
+    // The onclick functions are located in site-nav.hbs
+    if (url == "/"){
+        navItem = `<li class="nav-${slug} ${navCurrent}" onclick="updateLang(true)"><a href="${url}">${label}</a></li>`;
+    }
+    // If the URL is /fr/ we know the language should be french
+    else if (url == "/fr/"){
+        navItem = `<li class="nav-${slug} ${navCurrent}" onclick="updateLang(false)"><a href="${url}">${label}</a></li>`;
+    }
+
     return new SafeString(navItem);
 };
